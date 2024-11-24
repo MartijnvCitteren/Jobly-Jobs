6d3ed9bf606a0853a19dcb95617ef707a83b4f96
martijnvancitteren <martijnvancitteren@live.nl>
2024-11-24 15:40:17 +0100

    first commit

---
+++ src/main/java/com/jobly_jobs/JoblyJobsApplication.java
@@ -0,0 +1,13 @@
+package com.jobly_jobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoblyJobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoblyJobsApplication.class, args);
+	}
+
+}
