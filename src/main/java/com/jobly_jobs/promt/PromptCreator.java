7ad822d83788e6e62f759b5fe4e20fc3ae06e7cc
martijnvancitteren <martijnvancitteren@live.nl>
2025-03-02 18:36:38 +0100

    Updated promt

--- src/main/java/com/jobly_jobs/promt/PromptCreator.java
+++ src/main/java/com/jobly_jobs/promt/PromptCreator.java
@@ -1,50 +1,49 @@
 package com.jobly_jobs.promt;
 
 import com.jobly_jobs.domain.dto.request.JobCreationRequestDto;
-import com.jobly_jobs.domain.entity.Prompt;
 import com.jobly_jobs.domain.enums.WritingStyle;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PromptCreator {
 
-//    public Prompt createPrompt(JobCreationRequestDto inputDto) {
-//        String genericContext = createBasicPrompt(inputDto);
-//        Prompt prompt = new Prompt();
-//        prompt.setGenericContext(genericContext);
-//        prompt.setCompanyDescription(createCompanyDescription(inputDto, genericContext));
-//        prompt.setTeamDescription(createTeamDescription(inputDto, genericContext));
-//        prompt.setDayToDayDescription(createDateToDayDescription(inputDto, genericContext));
-//        prompt.setJobDescription(createJobDescription(inputDto, genericContext));
-//        prompt.setJobUniqueSellingPoints(createJobUniqueSellingPoints(inputDto, genericContext));
-//        prompt.setRequirements(createRequirements(inputDto, genericContext));
-//        prompt.setOffer(createOffer(inputDto, genericContext));
-//        prompt.setContactInformation(createContactInformation(inputDto, genericContext));
-//        prompt.setSummary(createSummary(inputDto, genericContext));
-//        return prompt;
-//    }
-
     public String createPrompt(JobCreationRequestDto inputDto) {
         String genericContext = createBasicPrompt(inputDto);
         StringBuilder sb = new StringBuilder();
         sb.append(genericContext);
-        sb.append(createCompanyDescription(inputDto, genericContext));
-        sb.append(createTeamDescription(inputDto, genericContext));
-        sb.append(createDateToDayDescription(inputDto, genericContext));
-        sb.append(createJobDescription(inputDto, genericContext));
-        sb.append(createJobUniqueSellingPoints(inputDto, genericContext));
-        sb.append(createRequirements(inputDto, genericContext));
-        sb.append(createOffer(inputDto, genericContext));
-        sb.append(createContactInformation(inputDto, genericContext));
-        sb.append(createSummary(inputDto, genericContext));
+        sb.append(createCompanyDescription(inputDto));
+        sb.append(createTeamDescription(inputDto));
+        sb.append(createDateToDayDescription(inputDto));
+        sb.append(createJobDescription(inputDto));
+        sb.append(createJobUniqueSellingPoints(inputDto));
+        sb.append(createRequirements(inputDto));
+        sb.append(createOffer(inputDto));
+        sb.append(createContactInformation(inputDto));
+        sb.append(createSummary(inputDto));
         return sb.toString();
     }
 
     private String createBasicPrompt(JobCreationRequestDto inputDto) {
         StringBuilder sb = new StringBuilder();
-        sb.append("Act like an experienced recruiter that has great writing skills. Your task is to write a good vacancy" +
-                "text that is appealing. As a write you understand the job market,  know a lot about SEO and " +
-                "how consumer psychology works. You only write a specific part of this job description. In this firs lines" +
+        sb.append("Act like an experienced recruiter that has great vacancy writing skills. Your task is to write a good vacancy" +
+                "text that is appealing to applicants who have the job title you try to attract. " +
+                "As an experienced you understand the job market,  know a lot about SEO and " +
+                "how consumer psychology works. You return the vacancy text in a structured data format. This format contains " +
+                " The following elements: " +
+                "1. Summary: a short summary of the job, this summary applies the AIDA model and has the goal to grabs someonse attention. " +
+                " and make someone interested to keep on reading. " +
+                "2. Company description: a short description of the company. Explain here what they do and why they do it" +
+                "3. Team description: a short description of the team. Explain what the team looks like and what makes it interesting to work with them" +
+                "4. Day to day description: a short description of the day to day activities. Explain what the job looks like on a daily basis" +
+                " and what challenges and opportunities are." +
+                "5. Job description: a short description of the job. Explain what the job is about and what makes it interesting to do." +
+                "6. Job unique selling points: a short description of the unique selling points of the job. Explain what makes this job unique and interesting." +
+                "7. Requirements: a short description of the requirements for the job. Explain what the company is looking for in a candidate." +
+                "This list should be between 4 and 6 requirements. Where the most important skills are listed first." +
+                "8. Offer: a list description of the offer for the job. Explain what the company has to offer to the candidate." +
+                "This list should be between 5 and 7 job benefits. Where the most important offers are listed first." +
+                "9. Contact information: a short description of the contact information for the job. Explain how to apply for the job." +
+                "Keep this structure in mind while writing the vacacytext and make sure there are NOT any repetitions in the text. " +
                 "you get some basic information about the job. If you don't think you need the information for the " +
                 "specific part you are writing, you can ignore it. ");
         sb.append("There is specific information you need to use in this text. The company name is: ");
@@ -61,15 +60,17 @@ public class PromptCreator {
 
     }
 
-    private String createCompanyDescription(JobCreationRequestDto inputDto, String genericContext) {
+    private String createCompanyDescription(JobCreationRequestDto inputDto) {
         StringBuilder sb = new StringBuilder();
-        sb.append(genericContext);
         sb.append("In this case write a company description and consider this " +
                 "information as most important: the company is: ");
         sb.append(inputDto.generalInfo().companyName());
-        sb.append(" If you know what this company does, you can write about that. If you don't know this company DO NOT make up information.");
-        sb.append(" Don't write about the company culture or values if you don't know them. If you know feel free to do so.");
-        sb.append(" If you don't know the company, you can write about the industry the company is in. you can this based on the summary of the job: ");
+        sb.append(" First act like as a human recruiter and search the internet if you can find information about this company. " +
+                "Make sure you find a website that's up to date. And written in the language the vacancy text is written in." +
+                " If you can't find information about the company, you can use information that you already know. " +
+                "If you don't know this company DO NOT make up information. Don't write about the company culture or " +
+                "values if you don't know them. If you do know feel add them in the text. If you don't know the company," +
+                " you can write about the industry the company is in. you can this based on the summary of the job");
         sb.append(inputDto.jobSummary());
         sb.append(" and the team description: ");
         sb.append(inputDto.teamDescription());
@@ -77,9 +78,8 @@ public class PromptCreator {
         return sb.toString();
     }
 
-    private String createTeamDescription(JobCreationRequestDto inputDto, String genericContext) {
+    private String createTeamDescription(JobCreationRequestDto inputDto) {
         StringBuilder sb = new StringBuilder();
-        sb.append(genericContext);
         sb.append("In this case write a team description and consider this " +
                 "information as most important: the team looks the following: :");
         sb.append(inputDto.teamDescription());
@@ -91,9 +91,8 @@ public class PromptCreator {
         return sb.toString();
     }
 
-    private String createDateToDayDescription(JobCreationRequestDto inputDto, String genericContext) {
+    private String createDateToDayDescription(JobCreationRequestDto inputDto) {
         StringBuilder sb = new StringBuilder();
-        sb.append(genericContext);
         sb.append("In this case write a day to day description and consider this " +
                 "information as most important: the day to day activities are: :");
         sb.append(inputDto.tasks());
@@ -105,9 +104,8 @@ public class PromptCreator {
         return sb.toString();
     }
 
-    private String createJobDescription(JobCreationRequestDto inputDto, String genericContext) {
+    private String createJobDescription(JobCreationRequestDto inputDto) {
         StringBuilder sb = new StringBuilder();
-        sb.append(genericContext);
         sb.append("In this case write a job description and consider this " +
                 "information as most important: the job description is: :");
         sb.append(inputDto.tasks());
@@ -120,9 +118,8 @@ public class PromptCreator {
         return sb.toString();
     }
 
-    private String createJobUniqueSellingPoints(JobCreationRequestDto inputDto, String genericContext) {
+    private String createJobUniqueSellingPoints(JobCreationRequestDto inputDto) {
         StringBuilder sb = new StringBuilder();
-        sb.append(genericContext);
         sb.append("In this case write a job's unique selling points. There should a maximum of four USP's " +
                 " and consider this " +
                 "information as most important: the job unique selling points are: :");
@@ -135,9 +132,8 @@ public class PromptCreator {
         return sb.toString();
     }
 
-    private String createRequirements(JobCreationRequestDto inputDto, String genericContext) {
+    private String createRequirements(JobCreationRequestDto inputDto) {
         StringBuilder sb = new StringBuilder();
-        sb.append(genericContext);
         sb.append("In this case write the requirements for the job and consider this " +
                 "information as most important: the requirements are: :");
         sb.append(inputDto.skills());
@@ -150,9 +146,8 @@ public class PromptCreator {
         return sb.toString();
     }
 
-    private String createOffer(JobCreationRequestDto inputDto, String genericContext) {
+    private String createOffer(JobCreationRequestDto inputDto) {
         StringBuilder sb = new StringBuilder();
-        sb.append(genericContext);
         sb.append("In this case write the offer for the job and consider this " +
                 "information as most important: the offer is: :");
         sb.append(inputDto.generalInfo().minSalary());
@@ -168,9 +163,8 @@ public class PromptCreator {
         return sb.toString();
     }
 
-    private String createContactInformation(JobCreationRequestDto inputDto, String genericContext) {
+    private String createContactInformation(JobCreationRequestDto inputDto) {
         StringBuilder sb = new StringBuilder();
-        sb.append(genericContext);
         sb.append("In this case write the contact information for the job and consider this " +
                 "information as most important: the contact information is: :");
         sb.append(inputDto.generalInfo().companyName());
@@ -178,36 +172,30 @@ public class PromptCreator {
         return sb.toString();
     }
 
-    private String createSummary(JobCreationRequestDto inputDto, String genericContext) {
+    private String createSummary(JobCreationRequestDto inputDto) {
         StringBuilder sb = new StringBuilder();
-        sb.append(genericContext);
         sb.append("In this case write a summary for the job and consider this " +
                 "information as most important: the summary is: :");
         sb.append(inputDto.jobSummary());
-        sb.append(" make this an alinea of 2-3 sentences. But also use the following information: ");
-        sb.append(inputDto.tasks());
-        sb.append(" and ");
-        sb.append(inputDto.skills());
-        sb.append(" and ");
-        sb.append(inputDto.teamDescription());
+        sb.append(" make this an alinea of 2-3 sentences. But also use the information that is written before.");
         return sb.toString();
     }
 
 
     private String writingStyleExample(WritingStyle style) {
-        switch (style) {
-            case FORMAL:
-                return "You have to write in a formal style. This means you have to use formal language and avoid using contractions. For example, instead of writing 'you're' you have to write 'you are'.";
-            case BUSINESS_CASUAL:
-                return "You have to write in a business casual style. This means you have to use a friendly tone and avoid using jargon. For example, instead of writing 'synergy' you have to write 'working together'.";
-            case CASUAL:
-                return "You have to write in a casual style. This means you have to use a conversational tone and avoid using complex words. For example, instead of writing 'utilize' you have to write 'use'.";
-            case CREATIVE:
-                return "You have to write in a creative style. This means you have to use imaginative language and avoid using cliches. For example, instead of writing 'think outside the box' you have to write 'be innovative'.";
-            case TECHNICAL:
-                return "You have to write in a technical style. This means you have to use precise language and avoid using vague terms. For example, instead of writing 'a lot' you have to write 'many'. Focus on technical terms and inovative solutions.";
-            default:
-                return "Write in a style that is easy to read and understand. Use short sentences and paragraphs. Aim to write on B2 or C1 level.";
-        }
+        return switch (style) {
+            case FORMAL ->
+                    "You have to write in a formal style. This means you have to use formal language and avoid using contractions. For example, instead of writing 'you're' you have to write 'you are'.";
+            case BUSINESS_CASUAL ->
+                    "You have to write in a business casual style. This means you have to use a friendly tone and avoid using jargon. For example, instead of writing 'synergy' you have to write 'working together'.";
+            case CASUAL ->
+                    "You have to write in a casual style. This means you have to use a conversational tone and avoid using complex words. For example, instead of writing 'utilize' you have to write 'use'.";
+            case CREATIVE ->
+                    "You have to write in a creative style. This means you have to use imaginative language and avoid using cliches. For example, instead of writing 'think outside the box' you have to write 'be innovative'.";
+            case TECHNICAL ->
+                    "You have to write in a technical style. This means you have to use precise language and avoid using vague terms. For example, instead of writing 'a lot' you have to write 'many'. Focus on technical terms and inovative solutions.";
+            default ->
+                    "Write in a style that is easy to read and understand. Use short sentences and paragraphs. Aim to write on B2 or C1 level.";
+        };
     }
 }
